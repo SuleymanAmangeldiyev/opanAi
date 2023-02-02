ba477821aaedb936a506b563b050e19d81df6432
Suleyman <suleyman20071998@gmail.com>
2023-02-02 20:08:09 +0200

    the server side

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://codex-im0y.onrender.com/", {
+  const response = await fetch("https://suleyman-open-ai.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
